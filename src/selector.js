edd4e1b13fc9cffecebe400539607b1e0e5a8f46
Smefinka <smefinka1@gmail.com>
2023-10-01 19:18:30 +0200

    nn

--- src/selector.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-import { selectContacts } from './reducer';
-
-export const selectMemoizedContacts = createSelector(
-  [selectContacts],
-  (contacts) => contacts
-);
